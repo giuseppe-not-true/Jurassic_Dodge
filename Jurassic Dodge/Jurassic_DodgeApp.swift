8488eba41d40271dc6e380db549cbdd6a234d615
Giuseppe Falso <giuseppeefalso@gmail.com>
2022-05-11 20:19:21 +0200

    started putting scenephase variable

--- Jurassic Dodge/Jurassic_DodgeApp.swift
+++ Jurassic Dodge/Jurassic_DodgeApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct Jurassic_DodgeApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onChange(of: scenePhase) { newPhase in
+                    if newPhase == .active {
+                        print("Active")
+                    } else if newPhase == .inactive {
+                        print("Inactive")
+                    } else if newPhase == .background {
+                        print("Background")
+                    }
+                }
         }
     }
 }
